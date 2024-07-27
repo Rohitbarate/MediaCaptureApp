a8816f6f23edb5f77f655145775464f9592640ea
Rohit Rajendra Barate <rohitbarate100@gmail.com>
2024-07-28 03:50:33 +0530

    redux store setup done

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,11 @@
 import {configureStore} from '@reduxjs/toolkit';
 import authReducer from './slices/authSlice';
+import mediaReducer from './slices/mediaSlice';
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
+    media: mediaReducer,
   },
 });
 
